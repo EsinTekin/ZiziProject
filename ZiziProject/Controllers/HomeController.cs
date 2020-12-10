c27e2043d0769c582f7c3724136222fa37483cc7
Esin Tekin <esnntekin@gmail.com>
2020-12-11 01:02:04 +0300

    Update in Models and Views

--- ZiziProject/Controllers/HomeController.cs
+++ ZiziProject/Controllers/HomeController.cs
@@ -41,7 +41,35 @@ namespace ZiziProject.Controllers
         }
         public IActionResult Eserler()
         {
-            return View();
+            // repository nesnesinden bir örnek alıyoruz
+            PostRepository repo = new PostRepository();
+            // bir post nesnesi oluşturuyoruz
+            var obj = new Post
+            {
+                id = 1,
+                title = "İlk Blog Başlığı",
+                content = "İlk blog içeriği"
+            };
+            // nesneyi kayıt ediyoruz
+            repo.AddPost(obj);
+            // ikinci bir post nesnesi oluşturuyoruz
+            var obj2 = new Post
+            {
+                id = 2,
+                title = "İkinci Blog Başlığı",
+                content = "İkinci blog içeriği"
+            };
+            // obj2 nesnesini kayıt ediyoruz
+            repo.AddPost(obj2);
+            // obj3 içerisine id'si 1 olan nesneyi atıyoruz
+            var obj3 = repo.GetPost(1);
+            // ViewData ile obj3 içindeki değeri html içerisinde çağırmak için kayıt ediyoruz
+            ViewData["secilenNesne"] = obj3;
+
+            // View'e farklı veri tipleri geçerek cshtml içerisinde
+            // ekrana basabiliriz
+            return View(repo.GetAllPosts());
+           // return View();
         }
         public IActionResult Iletisim()
         {
