77f10bb6bb2768d7febccb81a80fb899fc2ac000
Esin Tekin <esnntekin@gmail.com>
2020-12-18 02:05:56 +0300

    Update views

--- ZiziProject/Controllers/HomeController.cs
+++ ZiziProject/Controllers/HomeController.cs
@@ -62,10 +62,32 @@ namespace ZiziProject.Controllers
             };
             // obj2 nesnesini kayıt ediyoruz
             repo.AddPost(obj2);
+
+            var obj3 = new Post
+            {
+                title = "Üçüncü Blog Başlığı",
+                content = "Üçüncü blog içeriği"
+            };
+            repo.AddPost(obj3);
+            var obj4 = new Post
+            {
+                title = "4. Blog Başlığı",
+                content = "4. blog içeriği"
+            };
+            repo.AddPost(obj4);
+            var obj6 = new Post
+            {
+                title = "6. Blog Başlığı",
+                content = "6. blog içeriği"
+            };
+            repo.AddPost(obj6);
+            ViewData["secilenNesne"] = obj6;
+            //var obj5 = repo.GetPost(16);
+            //ViewData["secilenNesne"] = obj5;
             // obj3 içerisine id'si 1 olan nesneyi atıyoruz
-            var obj3 = repo.GetPost(1);
-            // ViewData ile obj3 içindeki değeri html içerisinde çağırmak için kayıt ediyoruz
-            ViewData["secilenNesne"] = obj3;
+            //var obj3 = repo.GetPost(1);
+            //// ViewData ile obj3 içindeki değeri html içerisinde çağırmak için kayıt ediyoruz
+            //ViewData["secilenNesne"] = obj3;
 
             // View'e farklı veri tipleri geçerek cshtml içerisinde
             // ekrana basabiliriz
